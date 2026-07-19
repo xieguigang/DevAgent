d147b9de73608bc091f7949fa9f9a258d996cbec
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-19 22:24:54 +0800

    open settings page

--- src/Vallina/Application/RibbonMenu.vb
+++ src/Vallina/Application/RibbonMenu.vb
@@ -28,7 +28,7 @@ Module RibbonMenu
     End Sub
 
     Public Sub OpenSettingsPage()
-        Call CommonRuntime.ShowSingleDocument(Of FormHtmlViewer)().SetUrl($"http://127.0.0.1:{Workbench.port}/settings.html", New SettingsPage).SetTitle("Title")
+        Call CommonRuntime.ShowSingleDocument(Of FormHtmlViewer)().SetUrl($"http://127.0.0.1:{Workbench.port}/settings.html", New SettingsPage).SetTitle("Settings")
     End Sub
 
     Public Sub OpenEditor()
