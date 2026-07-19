ee65f1cd827ba2766b1ad5f47773d0c01bbf0ffe
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-19 16:24:12 +0800

    config http

--- src/Vallina/Application/Workbench.vb
+++ src/Vallina/Application/Workbench.vb
@@ -7,4 +7,24 @@
 
     End Sub
 
+    Private Function GetWebRoot() As String
+        If CheckDevelopmentMode() Then
+            _wwwroot = "G:\DevAgent\code-editor"
+        Else
+            _wwwroot = App.HOME & "/apps"
+        End If
+
+        Return wwwroot
+    End Function
+
+    Private Function CheckDevelopmentMode() As Boolean
+        Dim home As String = App.HOME.ToLower.Replace("\", "/").Replace("//", "/")
+
+        If home.StartsWith("g:\devagent") Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
 End Module
--- src/Vallina/Pages/FormEditor.vb
+++ src/Vallina/Pages/FormEditor.vb
@@ -1,8 +1,13 @@
 ﻿Imports Galaxy.Workbench
+Imports Microsoft.Web.WebView2.Core
 
 Public Class FormEditor
 
     Private Async Sub FormEditor_Load(sender As Object, e As EventArgs) Handles Me.Load
         Await WebViewLoader.Init(WebView21)
+    End Sub
+
+    Private Sub WebView21_CoreWebView2InitializationCompleted(sender As Object, e As CoreWebView2InitializationCompletedEventArgs) Handles WebView21.CoreWebView2InitializationCompleted
+        Call WebView21.CoreWebView2.Navigate($"http://localhost:{Workbench.port}/index.html")
     End Sub
 End Class
