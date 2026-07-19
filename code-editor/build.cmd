08b902f45371f8eab7d23a05cffd61a7a0d7503b
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-19 18:15:10 +0800

    Update build.cmd

--- code-editor/build.cmd
+++ code-editor/build.cmd
@@ -1,3 +1,7 @@
 @echo off
 
-npx esbuild src/main.ts --bundle --minify --outfile=dist/editor.bundle.js --format=iife
+REM module=es6
+REM npx esbuild src/main.ts --bundle --minify --outfile=dist/editor.bundle.js --format=iife
+
+REM module=none
+tsc
