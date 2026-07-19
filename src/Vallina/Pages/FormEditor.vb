b479c970a0b4e6274f8393e7e8a8e30f471456cd
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-19 16:17:59 +0800

    initialize of the webview

--- src/Vallina/Pages/FormEditor.vb
+++ src/Vallina/Pages/FormEditor.vb
@@ -1,3 +1,8 @@
-﻿Public Class FormEditor
+﻿Imports Galaxy.Workbench
 
+Public Class FormEditor
+
+    Private Async Sub FormEditor_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Await WebViewLoader.Init(WebView21)
+    End Sub
 End Class
