ba29761566e5050579e716fc37c451f4a056a379
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-19 17:16:31 +0800

    add start page

---
+++ src/Vallina/Pages/FormStartPage.vb
@@ -0,0 +1,13 @@
+﻿Imports Galaxy.Workbench
+Imports Microsoft.Web.WebView2.Core
+
+Public Class FormStartPage
+
+    Private Async Sub FormStartPage_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Await WebViewLoader.Init(WebView21)
+    End Sub
+
+    Private Sub WebView21_CoreWebView2InitializationCompleted(sender As Object, e As CoreWebView2InitializationCompletedEventArgs) Handles WebView21.CoreWebView2InitializationCompleted
+        Call WebView21.CoreWebView2.Navigate($"http://localhost:{Workbench.port}/startpage.html")
+    End Sub
+End Class
