ab824324d027e086bf11df67f34c24fc6a527cad
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-19 18:19:54 +0800

    try to hide the status bar of the web editor

--- src/Vallina/Pages/FormEditor.vb
+++ src/Vallina/Pages/FormEditor.vb
@@ -10,4 +10,8 @@ Public Class FormEditor
     Private Sub WebView21_CoreWebView2InitializationCompleted(sender As Object, e As CoreWebView2InitializationCompletedEventArgs) Handles WebView21.CoreWebView2InitializationCompleted
         Call WebView21.CoreWebView2.Navigate($"http://localhost:{Workbench.port}/index.html")
     End Sub
+
+    Private Async Sub WebView21_NavigationCompleted(sender As Object, e As CoreWebView2NavigationCompletedEventArgs) Handles WebView21.NavigationCompleted
+        Await WebView21.ExecuteScriptAsync("document.getElementByID('statusbar').style.display='none';")
+    End Sub
 End Class
