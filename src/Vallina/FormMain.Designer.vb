d09d4d650b1e7eddea4f96c4de8db8201634d2da
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-19 15:36:11 +0800

    config components

--- src/Vallina/FormMain.Designer.vb
+++ src/Vallina/FormMain.Designer.vb
@@ -22,10 +22,65 @@ Partial Class FormMain
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Ribbon1 = New RibbonLib.Ribbon()
+        DockPanel1 = New Microsoft.VisualStudio.WinForms.Docking.DockPanel()
+        StatusStrip1 = New StatusStrip()
+        ToolStripStatusLabel1 = New ToolStripStatusLabel()
+        StatusStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Ribbon1
+        ' 
+        Ribbon1.Location = New Point(0, 0)
+        Ribbon1.Name = "Ribbon1"
+        Ribbon1.ResourceIdentifier = Nothing
+        Ribbon1.ResourceName = Nothing
+        Ribbon1.ShortcutTableResourceName = Nothing
+        Ribbon1.Size = New Size(1087, 116)
+        Ribbon1.TabIndex = 0
+        ' 
+        ' DockPanel1
+        ' 
+        DockPanel1.Dock = DockStyle.Fill
+        DockPanel1.Location = New Point(0, 116)
+        DockPanel1.Name = "DockPanel1"
+        DockPanel1.Size = New Size(1087, 464)
+        DockPanel1.TabIndex = 1
+        ' 
+        ' StatusStrip1
+        ' 
+        StatusStrip1.Items.AddRange(New ToolStripItem() {ToolStripStatusLabel1})
+        StatusStrip1.Location = New Point(0, 580)
+        StatusStrip1.Name = "StatusStrip1"
+        StatusStrip1.Size = New Size(1087, 22)
+        StatusStrip1.TabIndex = 2
+        StatusStrip1.Text = "StatusStrip1"
+        ' 
+        ' ToolStripStatusLabel1
+        ' 
+        ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        ToolStripStatusLabel1.Size = New Size(39, 17)
+        ToolStripStatusLabel1.Text = "Ready"
+        ' 
+        ' FormMain
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
-        Text = "Form1"
+        ClientSize = New Size(1087, 602)
+        Controls.Add(DockPanel1)
+        Controls.Add(StatusStrip1)
+        Controls.Add(Ribbon1)
+        Name = "FormMain"
+        Text = "Vallina Developer"
+        StatusStrip1.ResumeLayout(False)
+        StatusStrip1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
     End Sub
+
+    Friend WithEvents Ribbon1 As RibbonLib.Ribbon
+    Friend WithEvents DockPanel1 As Microsoft.VisualStudio.WinForms.Docking.DockPanel
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
 
 End Class
--- src/Vallina/Pages/FormEditor.Designer.vb
+++ src/Vallina/Pages/FormEditor.Designer.vb
@@ -24,15 +24,37 @@ Partial Class FormEditor
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        WebView21 = New Microsoft.Web.WebView2.WinForms.WebView2()
+        CType(WebView21, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
+        ' 
+        ' WebView21
+        ' 
+        WebView21.AllowExternalDrop = True
+        WebView21.CreationProperties = Nothing
+        WebView21.DefaultBackgroundColor = Color.White
+        WebView21.Dock = DockStyle.Fill
+        WebView21.Location = New Point(0, 0)
+        WebView21.Name = "WebView21"
+        WebView21.Size = New Size(1091, 726)
+        WebView21.TabIndex = 1
+        WebView21.ZoomFactor = 1R
         ' 
         ' FormEditor
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(800, 450)
+        ClientSize = New Size(1091, 726)
+        Controls.Add(WebView21)
+        DockAreas = Microsoft.VisualStudio.WinForms.Docking.DockAreas.Float Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockLeft Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockRight Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockTop Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.DockBottom Or Microsoft.VisualStudio.WinForms.Docking.DockAreas.Document
+        DoubleBuffered = True
         Name = "FormEditor"
+        ShowHint = Microsoft.VisualStudio.WinForms.Docking.DockState.Unknown
+        TabPageContextMenuStrip = DockContextMenuStrip1
         Text = "Visual Studio Code"
+        CType(WebView21, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
     End Sub
+
+    Friend WithEvents WebView21 As Microsoft.Web.WebView2.WinForms.WebView2
 End Class
