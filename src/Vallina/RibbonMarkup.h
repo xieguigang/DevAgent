4923f4425240d6c90c7498bc87b9133b67dd1a86
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-19 16:01:20 +0800

    create ribbon menu ui

---
+++ src/Vallina/RibbonMarkup.h
@@ -0,0 +1,48 @@
+// *****************************************************************************
+// * This is an automatically generated header file for UI Element definition  *
+// * resource symbols and values. Please do not modify manually.               *
+// *****************************************************************************
+
+#pragma once
+
+#define cmdButtonNew 2 
+#define cmdButtonNew_LabelTitle_RESID 60001
+#define cmdButtonNew_SmallImages_192__RESID 60002
+#define cmdButtonNew_LargeImages_192__RESID 60003
+#define cmdButtonExit 3 
+#define cmdButtonExit_LabelTitle_RESID 60004
+#define cmdButtonExit_SmallImages_192__RESID 60005
+#define cmdButtonExit_LargeImages_192__RESID 60006
+#define cmdButtonAbout 4 
+#define cmdButtonAbout_LabelTitle_RESID 60007
+#define cmdButtonAbout_SmallImages_192__RESID 60008
+#define cmdButtonAbout_LargeImages_192__RESID 60009
+#define cmdButtonSave 5 
+#define cmdButtonSave_LabelTitle_RESID 60010
+#define cmdButtonSave_SmallImages_192__RESID 60011
+#define cmdButtonSave_LargeImages_192__RESID 60012
+#define cmdButtonSaveAll 6 
+#define cmdButtonSaveAll_LabelTitle_RESID 60013
+#define cmdButtonSaveAll_SmallImages_192__RESID 60014
+#define cmdButtonSaveAll_LargeImages_192__RESID 60015
+#define cmdRibbonEditor 7 
+#define cmdRibbonEditor_LabelTitle_RESID 60016
+#define cmdMenuEditor 8 
+#define cmdMenuEditor_LabelTitle_RESID 60017
+#define cmdGroupEditor 9 
+#define cmdGroupEditor_LabelTitle_RESID 60018
+#define cmdButtonSaveAs 10 
+#define cmdButtonSaveAs_LabelTitle_RESID 60019
+#define cmdButtonSaveAs_SmallImages_192__RESID 60020
+#define cmdButtonSaveAs_LargeImages_192__RESID 60021
+#define cmdButtonOpen 11 
+#define cmdButtonOpen_LabelTitle_RESID 60022
+#define cmdButtonOpen_SmallImages_192__RESID 60023
+#define cmdButtonOpen_LargeImages_192__RESID 60024
+#define cmdGroupFile 12 
+#define cmdGroupFile_LabelTitle_RESID 60025
+#define cmdGroupSave 13 
+#define cmdGroupSave_LabelTitle_RESID 60026
+#define cmdMenuEdit 14 
+#define cmdMenuEdit_LabelTitle_RESID 60027
+#define InternalCmd2_LabelTitle_RESID 60028
