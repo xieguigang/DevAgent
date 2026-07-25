dcec93c2ad6d5a00be431f1f6fdfd687150afe91
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-26 01:24:07 +0800

    run language services

--- src/LanguageServer/Program.vb
+++ src/LanguageServer/Program.vb
@@ -1,7 +1,16 @@
 Imports Flute.Http.Configurations
+Imports Microsoft.VisualBasic.CommandLine
+Imports Microsoft.VisualBasic.CommandLine.Reflection
 
 Module Program
-    Sub Main(args As String())
-        Call New Services(8015, New Configuration).Run()
-    End Sub
+
+    Public Function Main(args As String()) As Integer
+        Return GetType(Program).RunCLI(App.CommandLine)
+    End Function
+
+    <ExportAPI("--listen")>
+    <Usage("--listen [--port=8015]")>
+    Public Function Run(Optional port As Integer = 8015, Optional args As CommandLine = Nothing) As Integer
+        Return New Services(port, New Configuration).Run()
+    End Function
 End Module
