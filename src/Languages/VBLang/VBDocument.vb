a0a738d7a9c85fa22fe0b6f76fd74db4ee56cbdd
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-28 23:44:19 +0800

    create  vb.net  source document model

---
+++ src/Languages/VBLang/VBDocument.vb
@@ -0,0 +1,51 @@
+﻿Public Class VBDocument
+
+    ''' <summary>
+    ''' relative path to the vbproj file
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property FileName As String
+    ''' <summary>
+    ''' namespace imports list
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property [Imports] As String()
+    ''' <summary>
+    ''' language symbols that parsed from current vb.net source file document text
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property Types As Dictionary(Of String, LanguageSymbolType)
+
+End Class
+
+Public Class [Imports]
+
+    ''' <summary>
+    ''' Imports XXX
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property [Namespace] As String
+    ''' <summary>
+    ''' Imports X = XXX
+    ''' </summary>
+    ''' <returns></returns>
+    Public Property [Alias] As String
+
+    Public Overrides Function ToString() As String
+        If [Alias].StringEmpty(, True) Then
+            Return $"Imports {[Namespace]}"
+        Else
+            Return $"Imports {[Alias]} = {[Namespace]}"
+        End If
+    End Function
+
+End Class
+
+Public Class VBProject
+
+    Public Property RootNamespace As String
+    Public Property AssemblyName As String
+    Public Property OutputType As String
+    Public Property CompileFiles As VBDocument()
+
+End Class
