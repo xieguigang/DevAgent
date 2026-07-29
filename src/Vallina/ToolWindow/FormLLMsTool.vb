ec8bd1bc7f2bdba6f62140fcea69ab925b243bb0
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-29 20:05:40 +0800

    config for run debug profile

--- src/Vallina/ToolWindow/FormLLMsTool.vb
+++ src/Vallina/ToolWindow/FormLLMsTool.vb
@@ -1,7 +1,10 @@
-﻿Public Class FormLLMsTool
+﻿Imports Galaxy.Workbench
+
+Public Class FormLLMsTool
 
     Private Sub FormLLMsTool_Load(sender As Object, e As EventArgs) Handles Me.Load
         WebView2llmui1.SetHost(Workbench.CreateLLM)
+        CommonRuntime.GetOutputWindow.AddLog("open llm", "load llm model: " & WebView2llmui1.llm)
     End Sub
 
     Public Async Function SetFileReference(filepath As String) As Task
