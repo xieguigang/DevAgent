f21c32b5e6fb3c7873166d8b7646d8b03e6c610d
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-29 23:16:57 +0800

    try to handling of the possible null reference errror

--- src/Vallina/Application/Javascript/StartupPage.vb
+++ src/Vallina/Application/Javascript/StartupPage.vb
@@ -5,5 +5,15 @@ Namespace Javascript
     <ComVisible(True)>
     Public Class StartupPage : Inherits BasePage
 
+        Public Async Function OpenProject() As Task
+            Using file As New OpenFileDialog With {
+               .Filter = "VisualBasic Project(*.vbproj)|*.vbproj"
+           }
+                If file.ShowDialog = DialogResult.OK Then
+                    Await Task.Run(Sub() Call RibbonMenu.OpenSolutionExplorer(file.FileName))
+                End If
+            End Using
+        End Function
+
     End Class
 End Namespace
