b7bb6757e80183e8271e551324c002e10197db8f
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-30 21:54:35 +0800

    starty llm agent with config data

--- src/Vallina/Pages/FormLLMsAgent.vb
+++ src/Vallina/Pages/FormLLMsAgent.vb
@@ -1,9 +1,19 @@
-﻿Public Class FormLLMsAgent
+﻿Imports Microsoft.VisualBasic.ApplicationServices
+
+Public Class FormLLMsAgent
 
     Private Sub FormLLMsAgent_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim llmconfig = Workbench.config.llm
+        Dim config As New DevAgent.AppConfig With {
+            .ApiKey = llmconfig.apiKey,
+            .MaxBuildFix = 15,
+            .MaxRunFix = 30
+        }
+        Dim tempfile As String = TempFileSystem.GetAppSysTempFile(".ini", sessionID:=App.PID, prefix:="appconfig-")
 
+        Call config.Save(tempfile)
 
-        ConsoleControl1.StartProcess(App.HOME & "/DevAgent.exe", Nothing)
+        ConsoleControl1.StartProcess(App.HOME & "/DevAgent.exe", $"--repl -c={tempfile.CLIPath} --url={llmconfig.endpoint.CLIToken} --model={llmconfig.model}")
     End Sub
 
     Private Sub ConsoleControl1_ProcessExisted() Handles ConsoleControl1.ProcessExisted
