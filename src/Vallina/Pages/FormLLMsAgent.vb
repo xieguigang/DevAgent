4cae9f0dc9ce9d187bfb3c6fa2c5e2d1c00a6fa4
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-30 21:30:45 +0800

    auto close page on process exit

--- src/Vallina/Pages/FormLLMsAgent.vb
+++ src/Vallina/Pages/FormLLMsAgent.vb
@@ -3,4 +3,8 @@
     Private Sub FormLLMsAgent_Load(sender As Object, e As EventArgs) Handles Me.Load
         ConsoleControl1.StartProcess(App.HOME & "/DevAgent.exe", Nothing)
     End Sub
+
+    Private Sub ConsoleControl1_ProcessExisted() Handles ConsoleControl1.ProcessExisted
+        Me.Close()
+    End Sub
 End Class
--- src/Vallina/ToolWindow/FormConsole.vb
+++ src/Vallina/ToolWindow/FormConsole.vb
@@ -3,4 +3,8 @@
     Private Sub FormConsole_Load(sender As Object, e As EventArgs) Handles Me.Load
         ConsoleControl1.StartProcess("cmd", Nothing)
     End Sub
+
+    Private Sub ConsoleControl1_ProcessExisted() Handles ConsoleControl1.ProcessExisted
+        Me.Close()
+    End Sub
 End Class
