2b1efc58225278457f673565486717c722105180
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-31 00:53:30 +0800

    make format of the vbcode

---
+++ src/DevAgent/src/SyntaxFormater.vb
@@ -0,0 +1,21 @@
+﻿Imports Microsoft.CodeAnalysis
+Imports Microsoft.CodeAnalysis.Formatting
+Imports Microsoft.CodeAnalysis.VisualBasic
+
+Public Module SyntaxFormater
+
+    Public Async Function FormatVBCode(sourceCode As String) As Task(Of String)
+        ' 创建 VB 的工作区
+        Dim workspace = New AdhocWorkspace()
+        ' 解析 VB 代码
+        Dim syntaxTree = VisualBasicSyntaxTree.ParseText(sourceCode)
+        ' 获取格式化后的语法树根节点
+        Dim root = Await syntaxTree.GetRootAsync()
+        Dim formattedRoot = Formatter.Format(root, workspace)
+
+        sourceCode = formattedRoot.ToString
+
+        Return sourceCode
+    End Function
+
+End Module
