f20e0f8e34b5ade44dd1426232bc266480b2f4ff
この中二病に爆焔を！ <xieguigang@metabolomics.ac.cn>
2026-07-30 21:28:54 +0800

    run dev agent consoole

--- src/Vallina/Pages/FormLLMsAgent.vb
+++ src/Vallina/Pages/FormLLMsAgent.vb
@@ -1,6 +1,6 @@
 ﻿Public Class FormLLMsAgent
 
     Private Sub FormLLMsAgent_Load(sender As Object, e As EventArgs) Handles Me.Load
-        ConsoleControl1.StartProcess("cmd", Nothing)
+        ConsoleControl1.StartProcess(App.HOME & "/DevAgent.exe", Nothing)
     End Sub
 End Class
